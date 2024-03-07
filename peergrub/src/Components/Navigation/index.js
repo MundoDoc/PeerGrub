84a91b130bdda518b1d78a299cd3afc1032f2300
titorivera0707 <titorivera0707@gmail.com>
2024-03-07 10:15:15 -0800

    Added a cart sidebar

--- peergrub/src/Components/Navigation/index.js
+++ peergrub/src/Components/Navigation/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import './index.css';
 import Logo from '../../Assets/Logo.png';
-import Cart from '../../Assets/Cart.png';
+import SideCart from '../SideCart';
 
 function Navigation() {
-    const handleEnter = (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            alert("You pressed enter! This does nothing for now.");
-        }
-    };
 
     return (
         <nav className="navbar navbar-expand-md bg-body-tertiary">
@@ -19,11 +13,6 @@ function Navigation() {
                     <a className="navbar-brand" href="/"><img src={Logo} alt="Logo" style={{ width: '50px' }} />Peer Grub</a>
                 </div>
                 
-                {/* Place search bar in its own container for easier manipulation */}
-                <div className="search-container">
-                    <input type="text" placeholder='Search...' onKeyDown={handleEnter} className="search-bar"/>
-                </div>
-                
                 {/* Group right-aligned items */}
                 <div className="nav-right">
                     <ul className="navbar-nav">
@@ -31,7 +20,7 @@ function Navigation() {
                             <a className="nav-link" href="/login">Login</a>
                         </li>
                         <li className="nav-item navUnique">
-                            <a className="nav-link" href="/shoppingcart"><img src={Cart} alt="Cart" style={{ width: '60px' }} /></a>
+                            <SideCart />
                         </li>
                     </ul>
                 </div>
---
+++ peergrub/src/Components/SideCart/index.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import IcecreamIcon from '@mui/icons-material/Icecream';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import Cart from '../../Assets/Cart.png';
+
+export default function SideCart() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['First Restaurant', 'Second Restaurant'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <RestaurantIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['Mock Item 1', 'Mock Item 2'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <FastfoodIcon /> : <IcecreamIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <a className="cartPage" href="/shoppingcart">Go to Shopping Cart</a>
+    </Box>
+  );
+
+  return (
+    <div>
+        <a className="nav-link" onClick={toggleDrawer(true)}><img src={Cart} alt="Cart" style={{ width: '60px' }} /></a>
+        <Drawer open={open} anchor="right" onClose={toggleDrawer(false)}>
+            {DrawerList}
+        </Drawer>
+    </div>
+  );
+}
