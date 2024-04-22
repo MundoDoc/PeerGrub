e2dd7436c76bf1f1c7084106ac69de1f5c093a86
Arnulfo Santillan Jr <59852318+arnulfosjr@users.noreply.github.com>
2024-04-22 14:10:55 -0700

    Added Rating, Rating Stats, admin class, new models, new form class, review textfield.

--- peerBack/backend/api/forms.py
+++ peerBack/backend/api/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Listing, Profile
+from .models import Listing, Profile, Rating
 
 class ProfileForm(forms.ModelForm):
   class Meta:
@@ -10,3 +10,8 @@ class ListingForm(forms.ModelForm):
   class Meta:
     model = Listing
     fields = ['Listing_Title','Listing_Image','Listing_Tags','Listing_Cost','Listing_Calories','Listing_Descr','Listing_Ingredients','allergen_choices']
+
+class RatingForm(forms.ModelForm):
+  class Meta:
+    model = Rating
+    fields = '__all__'
